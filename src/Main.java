94a7d66ddf13f122656a9deac6014391c31acb68
ebielle <eryblongo@gmail.com>
2024-04-19 13:29:28 +0200

    finished exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HashSet<String> colori = new HashSet<>();
+        String colore = "Verde";
+
+        System.out.println("Gli elementi dell'hashSet sono:");
+        for (String element : riempireColori(colori, colore)) {
+            System.out.println(element);
+        }
+
+        riempireColori(colori, colore).removeAll(colori);
+        System.out.println("L'hashSet \"colori\" è vuoto: " + colori.isEmpty());
+        System.out.println(colori);
+    }
+
+    public static HashSet<String> riempireColori(HashSet<String> colori, String colore) {
+        colori.add("Rosa");
+        colori.add("Rosso");
+        colori.add("Blu");
+        colori.add("Verde");
+
+        Iterator<String> iterator = colori.iterator();
+        while (iterator.hasNext()) {
+            if (colore.equals(iterator.next())) {
+                iterator.remove();
+            }
+        }
+        return colori;
     }
 }
