d4ce5b27349a800893841ef9b3ed865663a312a6
ebielle <eryblongo@gmail.com>
2024-04-25 15:31:36 +0200

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -6,30 +5,31 @@ public class Main {
     public static void main(String[] args) {
 
         HashSet<String> colori = new HashSet<>();
+
         String colore = "Verde";
 
-        System.out.println("Gli elementi dell'hashSet sono:");
-        for (String element : riempireColori(colori, colore)) {
-            System.out.println(element);
-        }
+        colori = riempireColori(colori);
 
-        riempireColori(colori, colore).removeAll(colori);
-        System.out.println("L'hashSet \"colori\" è vuoto: " + colori.isEmpty());
-        System.out.println(colori);
-    }
-
-    public static HashSet<String> riempireColori(HashSet<String> colori, String colore) {
-        colori.add("Rosa");
-        colori.add("Rosso");
-        colori.add("Blu");
-        colori.add("Verde");
 
         Iterator<String> iterator = colori.iterator();
         while (iterator.hasNext()) {
-            if (colore.equals(iterator.next())) {
+            if (colore == iterator.next()) {
                 iterator.remove();
             }
         }
-        return colori;
+
+        colori.removeAll(colori);
+        System.out.println("L'hashSet \"colori\" è vuoto: " + colori.isEmpty());
+        System.out.println(colori);
+        }
+
+        public static HashSet<String> riempireColori (HashSet < String > colori) {
+            colori.add("Rosa");
+            colori.add("Rosso");
+            colori.add("Blu");
+            colori.add("Verde");
+
+            return colori;
+
+        }
     }
-}
